8f1c12655c3c57cebd5e3895306aa739a9cc81ea
thakkarjeet008 <thakkarjeet008@gmail.com>
2026-07-14 23:11:20 +0530

    this ptr

---
+++ OOPS CONCEPTS/thispointer.cpp
@@ -0,0 +1,68 @@
+/*
+
+this is a special pointer in c++ that points to the current object .
+
+imp -  this-> is same as (*this).property 
+
+*/
+
+#include<iostream>
+#include<string>
+using namespace std;
+
+class Teacher 
+{
+    private:
+    double salary;
+
+    public:
+    // properties
+
+    // parameterized constructor
+
+        Teacher(string name, string department, string subject, double salary) // parameterized constructor
+        {
+            cout<<"\nParameterized constructor called\n"<<endl;
+            this->name = name;
+            this->department = department;
+            this->subject = subject;
+            this->salary = salary;
+        }
+    string name;
+    string department;
+    string subject;
+    
+        // setter functions
+        void setSalary(double s)
+        {
+            salary = s;
+        }
+
+        // getter functions
+        double getSalary()
+        {
+            return salary;
+        }
+    // methods / member functions
+
+    void changeDepartment(string newDepartment)
+    {
+        department = newDepartment;
+    }
+
+    void getInfo()
+    {
+        cout<<"Name: "<<name<<endl;
+        cout<<"Department: "<<department<<endl;
+        cout<<"Subject: "<<subject<<endl;
+        cout<<"Salary: "<<getSalary()<<endl;
+    }
+
+};
+
+int main()
+{
+    Teacher t1("JEET", "CSE", "OOPS", 50000);
+    t1.getInfo();
+    return 0;
+}
